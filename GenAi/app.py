b1b08d926a916136c78aa29117cff47c6caa6e11
EngkuAuni <engku.nha@gmail.com>
2025-08-21 10:17:28 +0800

    Deleted app.py from GenAI folder

--- GenAi/app.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-st.title("Streamlit Test")
-st.write("Wondering how to go on with you life?")
-
-struggle = st.text_input("Current Stuggle(s): ")
-if struggle:
-    st.success(f"Oof, {struggle} is tough")
