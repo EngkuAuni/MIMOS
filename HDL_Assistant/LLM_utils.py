fbb21af2efea5f9e2899d8efaa316ec103fba1b1
EngkuAuni <engku.nha@gmail.com>
2025-09-26 10:56:34 +0800

    modified:   LLM_utils.py

--- HDL_Assistant/LLM_utils.py
+++ HDL_Assistant/LLM_utils.py
@@ -4,15 +4,14 @@ import os
 from dotenv import load_dotenv
 from groq import Groq
 
-load_dotenv()
+load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), "Streamlit_env", ".env"))
 
 client = Groq(api_key=os.getenv("GROQ_API_KEY"))
 
-def call_llm(prompt, model="mistral-7b-8k"):
+def call_llm(prompt, model="deepseek-r1-distill-llama-70b"):
     response = client.chat.completions.create(
         model=model,
         messages=[{"role": "user", "content": prompt}],
         temperature=0.3
     )
     return response.choices[0].message.content
-
